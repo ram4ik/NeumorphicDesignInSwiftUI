38cf9c15617ca597c450725c6246c5e9ac69c7bc
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-05-09 17:50:37 +0300

    init

--- NeumorphicDesignInSwiftUI/ContentView.swift
+++ NeumorphicDesignInSwiftUI/ContentView.swift
@@ -10,7 +10,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color(red: 224/255, green: 229/255, blue: 236/255)
+            Button(action: {
+                
+            }) {
+                Image(systemName: "heart.fill")
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                    .shadow(color: Color.white, radius: 8, x: -8, y: -8)
+                    .shadow(color: Color(red: 163/255, green: 177/255, blue: 198/255), radius: 8, x: 9, y: 9)
+                    .padding()
+                    .background(Color(red: 224/255, green: 229/255, blue: 236/255))
+                    .cornerRadius(20)
+            }
+            .shadow(color: Color.white, radius: 8, x: -8, y: -8)
+            .shadow(color: Color(red: 163/255, green: 177/255, blue: 198/255), radius: 8, x: 9, y: 9)
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
